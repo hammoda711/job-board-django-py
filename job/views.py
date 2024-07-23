160e86116b2c7641055e9a8410cfebe00d4ec206
Mohamed Taher <mo.taher717@gmail.com>
2024-07-24 00:51:07 +0300

    add some logic in views and thier templates html files

--- job/views.py
+++ job/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 def available_jobs(request):
-    pass    
+    available_jobs = Job.objects.all()
+    context = {
+        'available_jobs':available_jobs
+    }   
+    return render(request,'job/available_jobs.html',context)
+
+
 
 def job_details(request,id):
-    pass
+    job_details = Job.objects.get(id=id)
+    context = {
+        'job_details':job_details
+    }
+    return render(request,'job/job_details.html',context)
