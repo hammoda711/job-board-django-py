fffbe3f7e545bb4f28c0ceaa282278c92a76080c
Mohamed Taher <mo.taher717@gmail.com>
2024-08-11 15:33:36 +0300

    dealing with pagination

--- job/views.py
+++ job/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator
 # Create your views here.
+
+
+
 def available_jobs(request):
     available_jobs = Job.objects.all()
+    paginator = Paginator(available_jobs, 1)  # Show 25 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'available_jobs':available_jobs
+        'available_jobs':page_obj
     }   
     return render(request,'job/available_jobs.html',context)
 
